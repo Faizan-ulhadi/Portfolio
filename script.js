a87a708ae4d137d809ce993161e105f2d56456cf
Faizan-ulhadi <fazexo69@gmail.com>
2024-05-28 21:19:53 +0500

    f

---
+++ script.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+=======
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+>>>>>>> 3365f70768b2e0857a34238477371806a2302232
+}
